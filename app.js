3ef5d70de6a52ee01662ba8af3ef76106ee0d1c2
yucheng880730 <yucheng2k13@gmail.com>
2021-09-29 14:00:52 +0800

    routing

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var myRouter = require("./routes/myroute");
 
 // 開始建立express模組，並命名為app變數
 var app = express();
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/my", myRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/myroute.js
@@ -0,0 +1,30 @@
+// 引入express模組
+var express = require("express");
+// 透過Router()方法新增一個路由器
+var router = express.Router();
+
+// router.get()傳入兩個參數
+// 第一個字串表示路徑
+// 第二個則是回調函數
+router.get("/", function (req, res) {
+  // function code
+});
+
+router.post("/", function (req, res) {
+  // function code
+});
+
+router.get("/hello", function (req, res) {
+  res.send("Hello Node.js");
+});
+
+// use postman to test
+router.post("/sum", function (req, res) {
+  var n1 = parseInt(req.body.num1);
+  var n2 = parseInt(req.body.num2);
+  var sum = n1 + n2;
+  // 加總並以json格式回覆
+  res.json({ mySum: sum });
+});
+
+module.exports = router;
